ef7c542778c3c55470d90bcf0ff93250ce312cd9
임병현 <koore12@gmail.com>
2024-05-24 16:18:48 +0900

    import fix2

--- slack_gpt_bot.py
+++ slack_gpt_bot.py
@@ -1,14 +1,16 @@
-import json
 import os
 import openai
-import state_management, commands
+from state_management import initialize_user_state, update_last_activity, check_session_timeout
+from commands import handle_message
 from slack_bolt import App
-from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+from dotenv import load_dotenv
 
+load_dotenv()
+# from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 # 환경 변수에서 Slack 토큰 및 OpenAI API 키 가져오기
 slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
-slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
-openai_api_key = os.environ["OPENAI_API_KEY"]
+slack_signing_secret = os.environ["SLACK_SIGNING_KEY"]
+openai_api_key = os.environ["OPEN_AI_API"]
 
 # Slack 앱 초기화
 app = App(token=slack_bot_token, signing_secret=slack_signing_secret)
@@ -23,19 +25,19 @@ def message_handler(message, say):
     user_message = message['text'].strip()
     
     # Initialize user state if not present
-    state_management.initialize_user_state(user_id, user_states)
-    state_management.update_last_activity(user_id, user_states)
+    initialize_user_state(user_id, user_states)
+    update_last_activity(user_id, user_states)
 
     # Check session timeout
-    if state_management.check_session_timeout(user_id, user_states):
+    if check_session_timeout(user_id, user_states):
         say("Session timed out due to inactivity. Starting a new session.")
         user_states.pop(user_id, None)
         return
 
     # Handle the user message
-    commands.handle_message(user_id, user_message, say, user_states)
+    handle_message(user_id, user_message, say, user_states)
 
-# Lambda 핸들러 생성
-def lambda_handler(event, context):
-    slack_handler = SlackRequestHandler(app)
-    return slack_handler.handle(event, context)
+# # Lambda 핸들러 생성
+# def lambda_handler(event, context):
+#     slack_handler = SlackRequestHandler(app)
+#     return slack_handler.handle(event, context)
